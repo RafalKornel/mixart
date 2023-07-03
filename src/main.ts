5ea28a887aeb50ec96f4bc11f2e7e2a5c4817b23
RafalKornel <rafal.kornel123@gmail.com>
2023-07-04 00:55:11 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,104 +1,228 @@
 import "./style.css";
-import { CanvasRenderer } from "./CanvasRenderer.ts";
-import { html } from "@arrow-js/core";
-import { Animation } from "./Animation.ts";
+import Phaser from "phaser";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <div id="animation-select"></div>
-    <canvas></canvas>
-  </div>
-`;
-
-enum CatState {
-  Idle1,
-  Idle2,
-  LickPaw,
-  Scratch,
-  Walk,
-  Run,
-  Sleep,
-  Tap,
-  Jump,
-  Spine,
-}
-
-const CAT_STATE_KEYS = Object.values(CatState).filter((v) => isNaN(Number(v)));
-
-const CAT_STATE_VALUES = Object.values(CatState).filter(
-  (v) => !isNaN(Number(v))
-);
-
-const CAT_FRAMES_COUNT: Record<CatState, number> = {
-  [CatState.Idle1]: 4,
-  [CatState.Idle2]: 4,
-  [CatState.LickPaw]: 4,
-  [CatState.Scratch]: 4,
-  [CatState.Walk]: 8,
-  [CatState.Run]: 8,
-  [CatState.Sleep]: 4,
-  [CatState.Tap]: 6,
-  [CatState.Jump]: 7,
-  [CatState.Spine]: 8,
+const gameSettings = {
+  playerSpeed: 1,
 };
 
-class Game extends CanvasRenderer {
-  private _animation: Animation<CatState>;
+enum Scene {
+  Loading = "loadingScene",
+  Example = "exampleScene",
+}
 
-  constructor(canvas: HTMLCanvasElement) {
-    super(canvas);
+class Cat extends Phaser.Physics.Matter.Sprite {
+  private _isTouchingGround: boolean;
 
-    this._animation = new Animation<CatState>({
-      initialAnimation: CatState.Idle1,
-      framesCount: CAT_FRAMES_COUNT,
-      frameHeight: 32,
-      frameWidth: 32,
-      spritesheetURI: "/cat.png",
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
+    super(scene.matter.world, x, y, texture, undefined, {
+      friction: 0.5,
+      mass: 1,
     });
+
+    scene.add.existing(this);
+
+    this.setFixedRotation();
+
+    // const catSprite = scene.matter.add.sprite(10, 10, "idle", undefined, {});
+
+    scene.anims.create({
+      key: "idle_anim",
+      frames: this.anims.generateFrameNumbers("idle"),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.play("idle_anim");
+
+    this._isTouchingGround = false;
+
+    scene.matter.world.on("collisionactive", () => {
+      this._isTouchingGround = true;
+    });
+
+    scene.events.on("update", this.update, this);
   }
 
-  changeCatState(animation: CatState) {
-    this._animation.setAnimation(animation);
+  get isTouchingGround() {
+    return this._isTouchingGround;
   }
 
-  loop(): void {
-    this._animation.increaseInternalCounter();
+  set isTouchingGround(val: boolean) {
+    this._isTouchingGround = val;
+  }
 
-    if (this._animation.shouldAnimate()) {
-      this._animation.nextFrame();
-    }
+  // update() {}
+}
 
-    this.ctx.drawImage(
-      this._animation.image,
-      this._animation.offset.y,
-      this._animation.offset.x,
-      this._animation.frameDimension.y,
-      this._animation.frameDimension.x,
-      0, // pos x
-      0, // pos y
-      this._animation.frameDimension.y * 8,
-      this._animation.frameDimension.x * 8
+enum BackgroundTile {
+  Background = "background",
+  Far = "far",
+  Close = "close",
+}
+
+class ParallaxBackground extends Phaser.GameObjects.Group {
+  private _bg: Phaser.GameObjects.TileSprite;
+  private _far: Phaser.GameObjects.TileSprite;
+  private _close: Phaser.GameObjects.TileSprite;
+
+  constructor(scene: Phaser.Scene) {
+    const bg = scene.add.tileSprite(
+      0,
+      0,
+      Number(scene.game.config.width),
+      Number(scene.game.config.height),
+      BackgroundTile.Background
     );
+    bg.setOrigin(0, 0);
+    bg.setScrollFactor(0);
+
+    const far = scene.add.tileSprite(
+      0,
+      0,
+      Number(scene.game.config.width),
+      Number(scene.game.config.height),
+      BackgroundTile.Far
+    );
+    far.setOrigin(0, 0);
+    far.setScrollFactor(0);
+
+    const close = scene.add.tileSprite(
+      0,
+      0,
+      Number(scene.game.config.width),
+      Number(scene.game.config.height),
+      BackgroundTile.Close
+    );
+    close.setOrigin(0, 0);
+    close.setScrollFactor(0);
+
+    super(scene, [bg, far, close]);
+
+    scene.add.existing(this);
+
+    // scene.events.on("update", this.update, this);
+
+    this._bg = bg;
+    this._close = close;
+    this._far = far;
+  }
+
+  public updateBackgroundPosition(camera: Phaser.Cameras.Scene2D.Camera) {
+    this._far.tilePositionX = camera.scrollX * 0.4;
+    this._close.tilePositionX = camera.scrollX * 0.6;
   }
 }
 
-const game = new Game(document.querySelector<HTMLCanvasElement>("canvas")!);
+class Example extends Phaser.Scene {
+  private _cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+  private _cat!: Cat;
+  private _parallax!: ParallaxBackground;
 
-game.start();
+  constructor() {
+    super(Scene.Example);
+  }
 
-html`
-  <select
-    @change="${(e: any) => {
-      const v = Number(e.target.value);
+  preload() {
+    this.load.spritesheet("idle", "assets/idle.png", {
+      frameWidth: 12,
+      frameHeight: 12,
+    });
 
-      if (!isNaN(v) && CAT_STATE_VALUES.includes(v)) {
-        game.changeCatState(v);
-      }
-    }}"
-  >
-    ${CAT_STATE_KEYS.map(
-      (key, value) =>
-        html`<option value="${CAT_STATE_VALUES[value]}">${key}</option>`
-    )}
-  </select>
-`(document.querySelector("#animation-select")!);
+    this.load.image("block", "assets/block.png");
+    this.load.image("platform", "assets/platform.png");
+
+    this.load.image(
+      BackgroundTile.Background,
+      "assets/background/parallax-mountain-bg.png"
+    );
+    this.load.image(
+      BackgroundTile.Far,
+      "assets/background/parallax-mountain-far.png"
+    );
+    this.load.image(
+      BackgroundTile.Close,
+      "assets/background/parallax-mountain-mountains.png"
+    );
+
+    this.load.image(
+      "bg_3",
+      "assets/background/parallax-mountain-foreground-trees.png"
+    );
+    this.load.image("bg_5", "assets/background/parallax-trees.png");
+  }
+
+  create() {
+    this._parallax = new ParallaxBackground(this);
+
+    this._cat = new Cat(this, 10, 10, "idle");
+
+    this._cursorKeys = this.input.keyboard?.createCursorKeys();
+
+    const ground = this.matter.add.image(100, 240, "platform", undefined, {
+      isStatic: true,
+      friction: 0,
+    });
+
+    // ground.scaleY = 2;
+
+    this.cameras.main.startFollow(this._cat, undefined, 0.2, 0.2, undefined, 30);
+  }
+
+  movePlayerManager() {
+    if (!this._cursorKeys) return;
+
+    if (this._cursorKeys.left.isDown) {
+      this._cat.setVelocityX(-gameSettings.playerSpeed);
+      this._cat.setFlipX(true);
+    } else if (this._cursorKeys.right.isDown) {
+      this._cat.setVelocityX(gameSettings.playerSpeed);
+      this._cat.setFlipX(false);
+    }
+
+    if (this._cursorKeys.up.isDown && this._cat.isTouchingGround) {
+      this._cat.isTouchingGround = false;
+      this._cat.applyForce(new Phaser.Math.Vector2(0, -0.02));
+    }
+  }
+
+  update() {
+    this.movePlayerManager();
+    this._parallax.updateBackgroundPosition(this.cameras.main);
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 320,
+  height: 160,
+  backgroundColor: "#000",
+  parent: "phaser-example",
+  pixelArt: true,
+  zoom: 2,
+  physics: {
+    default: "matter",
+    matter: {},
+  },
+  scene: [Example],
+};
+
+const game = new Phaser.Game(config);
+
+// game.start();
+
+// html`
+//   <select
+//     @change="${(e: any) => {
+//       const v = Number(e.target.value);
+
+//       if (!isNaN(v) && CAT_STATE_VALUES.includes(v)) {
+//         game.changeCatState(v);
+//       }
+//     }}"
+//   >
+//     ${CAT_STATE_KEYS.map(
+//       (key, value) =>
+//         html`<option value="${CAT_STATE_VALUES[value]}">${key}</option>`
+//     )}
+//   </select>
+// `(document.querySelector("#animation-select")!);
